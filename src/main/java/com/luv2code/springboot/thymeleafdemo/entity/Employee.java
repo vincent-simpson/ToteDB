8a7e0e5de75201feb15f3d6ce982f16dbe6d2641
vincent-simpson <vincefuse@gmail.com>
2019-06-10 16:43:33 -0400

    heroku

--- src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="monmouth.employee")
+@Table(name="employee", schema="monmouth")
 public class Employee {
 	
 	// define fields
