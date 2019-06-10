1b7312af7c0071e4b75d27cb7ebfc6a275dfa04b
vincent-simpson <vincefuse@gmail.com>
2019-06-10 18:32:08 -0400

    heroku

--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/employee/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/employee/EmployeeDAOHibernateImpl.java
@@ -99,11 +99,11 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 		
 		theQuery.executeUpdate();
 		
-		Query emptyTableQuery = currentSession.createQuery("from employee");
+		Query emptyTableQuery = currentSession.createQuery("from Employee");
 		List<Employee> temp = emptyTableQuery.getResultList();
 		
 		if(temp.isEmpty()) {
-			Query autoIncrementZero = currentSession.createSQLQuery("ALTER TABLE employee AUTO_INCREMENT=0");
+			Query autoIncrementZero = currentSession.createSQLQuery("ALTER TABLE monmouth.employee AUTO_INCREMENT=0");
 			autoIncrementZero.executeUpdate();
 		}
 		
