b4dde63a3b76ea732a974a6ed79e5d8cd15c86ba
vincent-simpson <vincefuse@gmail.com>
2019-06-10 15:32:50 -0400

    heroku

--- src/main/java/com/luv2code/springboot/thymeleafdemo/config/DatabaseConfig.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/config/DatabaseConfig.java
@@ -4,15 +4,26 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DatabaseConfig {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        URI dbUri;
+        
+        try {
+        	 dbUri = new URI(System.getenv("DATABASE_URL"));
+		} catch (URISyntaxException e) {
+			logger.error("Invalid DATABASE_URL");
+			return null;
+		}
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
