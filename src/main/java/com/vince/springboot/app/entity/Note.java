29baf1e2972ec2993640630026ff7a5c1b57c90c
vincent-simpson <vincefuse@gmail.com>
2019-06-23 23:54:03 -0400

    heroku

--- src/main/java/com/vince/springboot/app/entity/Note.java
+++ src/main/java/com/vince/springboot/app/entity/Note.java
@@ -16,8 +16,7 @@ import javax.persistence.Table;
 public class Note {
 	
 	@Id
-	@Column(name="id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@Column(name="note")
