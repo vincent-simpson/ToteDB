de05652579658dc09d4311eb6ee8f676000e7989
vincent-simpson <vincefuse@gmail.com>
2019-06-23 22:13:23 -0400

    heroku

--- src/main/java/com/vince/springboot/app/entity/Machine.java
+++ src/main/java/com/vince/springboot/app/entity/Machine.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 public class Machine {
 	
 	@Id
-	@Column(name="machineid")
+	@Column(name="machineId")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int machineId;
 	
