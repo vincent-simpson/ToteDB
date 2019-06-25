99e23dfacab7ee3bfaebf46cb48af21a3e879396
vincent-simpson <vincefuse@gmail.com>
2019-06-25 11:03:19 -0400

    Login authentication working

--- src/main/java/com/vince/springboot/app/config/SecurityConfig.java
+++ src/main/java/com/vince/springboot/app/config/SecurityConfig.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -24,8 +28,17 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:application.properties")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private Environment env;	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(securityDataSource());
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -64,6 +77,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.exceptionHandling().accessDeniedPage("/access-denied");
 
+	}
+	
+	//define bean for datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		BasicDataSource dataSource = new BasicDataSource();
+		
+		dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+		dataSource.setUrl(env.getProperty("spring.datasource.url"));
+		dataSource.setPassword(env.getProperty("spring.datasource.password"));
+		dataSource.setUsername(env.getProperty("spring.datasource.username"));
+		
+		return dataSource;
 	}
 	
 	
