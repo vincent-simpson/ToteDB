d1d3d91b03769e714fadbc6ff54d7aaaa866011e
Vince <vincefuse@gmail.com>
2019-06-29 17:56:32 -0400

    Need to fix employee information dropdown on machineList and masterMachineList

--- src/main/java/com/vince/springboot/app/controller/BettingAreaController.java
+++ src/main/java/com/vince/springboot/app/controller/BettingAreaController.java
@@ -24,105 +24,112 @@ import com.vince.springboot.app.entity.Machine;
 import com.vince.springboot.app.service.BettingAreaService;
 import com.vince.springboot.app.service.MachineService;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/bettingAreas")
 public class BettingAreaController {
-	
-	private BettingAreaService bettingAreaService;
-	private MachineService machineService;
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	private int test;
-	
-	@Autowired
-	public BettingAreaController(BettingAreaService bettingAreaService, MachineService machineService) {
-		this.bettingAreaService = bettingAreaService;
-		this.machineService = machineService;
-	}
-	
-	@GetMapping("/list")
-	public String bettingAreaList(Model theModel) {
-		List<BettingArea> bettingAreas = bettingAreaService.getAll();
-		BettingArea bettingArea = new BettingArea();
-		
-		List<Machine> machines = machineService.getAll();
-		Machine machine = new Machine();
-		
-		theModel.addAttribute("bettingArea", bettingArea);
-		theModel.addAttribute("bettingAreas", bettingAreas);
-		theModel.addAttribute("machines", machines);
-		theModel.addAttribute("machine", machine);
-		
-		return "machineList";
-	}
-	
-	@PostMapping("/save")
-	public String saveBettingArea(@ModelAttribute("bettingArea") BettingArea bettingArea,
-			@RequestParam("bettingAreaId2") int id) {
-		bettingArea.setId(id);
-		
-		bettingAreaService.save(bettingArea);
-		
-		return "redirect:/bettingAreas/list";
-	}
-	
-	@GetMapping("/delete")
-	public String delete(@RequestParam("bettingAreaId") int id) {
-		
-		bettingAreaService.delete(id);
-		
-		
-		return "redirect:/bettingAreas/list";
-	}
-	
-	
-	@PostMapping("/edit")
-	public String editBettingArea(@RequestParam("bettingAreaId") int id, Model model) {
-		
-		if(id == -1) {
-			model.addAttribute("bettingArea", new BettingArea());
-			
-			return "machineList :: modalAddBettingArea";
-		} else {
-			BettingArea bettingArea = bettingAreaService.getById(id);
-			
-			model.addAttribute("bettingArea", bettingArea);
-			
-			return "machineList :: modalAddBettingArea";
-		}
-		
-	}
-	
-	@PostMapping(value="/bindToModel")
-	public @ResponseBody void bindToModel(@RequestParam("bettingAreaId")int bettingAreaId, @ModelAttribute("machine") Machine machine, Model theModel) {
-		
-		logger.warn("bindToModel bettingAreaId = " + bettingAreaId);
-		
-		machine.setBettingArea(bettingAreaId);
-		test = bettingAreaId;
-		
-		logger.warn("test = bettingAreaId: " + test);
-		
-		theModel.addAttribute("machine", machine);
-		
-	}
-	
-	@GetMapping("/bind")
-	public String finalSaveMachine(@RequestParam("lsnNumber") int lsnNumber,
-									@RequestParam("serialNumber") String serialNumber,
-									@ModelAttribute("machine") Machine theMachine,
-									@RequestParam(value="machineId", required = false) int theId) {
-		
-		logger.warn("/bind   lsnNumber = " + lsnNumber + " :: serialNumber = " + serialNumber + " :: the machine id = " + theId);
-		
-		theMachine.setLsnNumber(lsnNumber);
-		theMachine.setSerialNumber(serialNumber);
-		theMachine.setBettingArea(test);
-		theMachine.setMachineId(theId);
-		logger.warn("bind machine to save: " + theMachine.toString());
-		machineService.save(theMachine);
-		
-		return "redirect:/bettingAreas/list";
-		
-	}
+
+    private BettingAreaService bettingAreaService;
+    private MachineService machineService;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private int test;
+
+    @Autowired
+    public BettingAreaController(BettingAreaService bettingAreaService, MachineService machineService) {
+        this.bettingAreaService = bettingAreaService;
+        this.machineService = machineService;
+    }
+
+    @GetMapping("/list")
+    public String bettingAreaList(Model theModel) {
+        List<BettingArea> bettingAreas = bettingAreaService.getAll();
+        BettingArea bettingArea = new BettingArea();
+
+        List<Machine> machines = machineService.getAll();
+        Machine machine = new Machine();
+
+        theModel.addAttribute("bettingArea", bettingArea);
+        theModel.addAttribute("bettingAreas", bettingAreas);
+        theModel.addAttribute("machines", machines);
+        theModel.addAttribute("machine", machine);
+
+        return "machineList";
+    }
+
+    @PostMapping("/save")
+    public String saveBettingArea(@ModelAttribute("bettingArea") BettingArea bettingArea,
+                                  @RequestParam("bettingAreaId2") int id) {
+        bettingArea.setId(id);
+
+        bettingAreaService.save(bettingArea);
+
+        return "redirect:/bettingAreas/list";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("bettingAreaId") int id) {
+
+        bettingAreaService.delete(id);
+
+
+        return "redirect:/bettingAreas/list";
+    }
+
+
+    @PostMapping("/edit")
+    public String editBettingArea(@RequestParam("bettingAreaId") int id, Model model) {
+
+        if (id == -1) {
+            model.addAttribute("bettingArea", new BettingArea());
+
+            return "machineList :: modalAddBettingArea";
+        } else {
+            BettingArea bettingArea = bettingAreaService.getById(id);
+
+            model.addAttribute("bettingArea", bettingArea);
+
+            return "machineList :: modalAddBettingArea";
+        }
+
+    }
+
+    @PostMapping("/bindToModel")
+    public @ResponseBody void bindToModel(@RequestParam("bettingAreaId") int bettingAreaId,
+                                          @ModelAttribute("machine") Machine machine,
+                                          Model theModel)
+    {
+
+        logger.warn("bindToModel bettingAreaId = " + bettingAreaId);
+
+        machine.setBettingArea(bettingAreaId);
+        test = bettingAreaId;
+
+        logger.warn("test = bettingAreaId: " + test);
+
+        theModel.addAttribute("machine", machine);
+
+    }
+
+    @GetMapping("/bind")
+    public String finalSaveMachine(@RequestParam("lsnNumber") int lsnNumber,
+                                   @RequestParam("serialNumber") String serialNumber,
+                                   @ModelAttribute("machine") Machine theMachine,
+                                   @RequestParam(value = "machineId", required = false) int theId,
+                                    HttpServletRequest request)
+    {
+
+        logger.warn("/bind   lsnNumber = " + lsnNumber + " :: serialNumber = " + serialNumber + " :: the machine id = " + theId);
+
+        theMachine.setLsnNumber(lsnNumber);
+        theMachine.setSerialNumber(serialNumber);
+        theMachine.setBettingArea(test);
+        theMachine.setMachineId(theId);
+        logger.warn("bind machine to save: " + theMachine.toString());
+        machineService.save(theMachine);
+
+        return "redirect:" + request.getHeader("Referer");
+
+    }
 
 }
