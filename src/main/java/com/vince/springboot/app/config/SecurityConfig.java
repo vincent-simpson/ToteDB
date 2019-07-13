b8ba9b234060b5b8938c7505e1c2a0b69a00774f
Vince <vincefuse@gmail.com>
2019-07-13 15:52:43 -0400

    Added error message if credentials are incorrect

--- src/main/java/com/vince/springboot/app/config/SecurityConfig.java
+++ src/main/java/com/vince/springboot/app/config/SecurityConfig.java
@@ -1,6 +1,14 @@
 package com.vince.springboot.app.config;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,19 +18,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
 /*
 This is the configuration file for Spring Security.
@@ -35,15 +33,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private final Environment env;
 
+	private CustomAuthFailureHandler customAuthFailureHandler;
+	
 	public SecurityConfig(Environment env) {
 		this.env = env;
 	}
-
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(securityDataSource());
-
-
 	}
 
 	@Override
@@ -66,13 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					response.sendRedirect("/"); // "/" is the request that maps to the main dashboard.
 				}
 			})
-			.failureHandler(new AuthenticationFailureHandler() {
-				@Override
-				public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-						AuthenticationException exception) throws IOException, ServletException {
-					response.sendRedirect("/login");
-				}
-			})
+			.failureHandler(customAuthFailureHandler)
 			.permitAll()
 			.and()
 			.logout()
